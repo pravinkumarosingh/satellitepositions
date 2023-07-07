e82e6de69f7d19ad0594ea357f5fb8d5ae2be96c
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-07-07 20:16:56 +0530

    initial commit

---
+++ src/main/java/com/project/satellitepositions/SatellitepositionsApplication.java
@@ -0,0 +1,13 @@
+package com.project.satellitepositions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatellitepositionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatellitepositionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/satellitepositions/SatellitepositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.satellitepositions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatellitepositionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
